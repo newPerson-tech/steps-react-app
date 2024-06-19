fd64210b541e6e55d75c7ead1e7ddc7f09db171f
Tetiana Paniot <tatyana.paniot@gmail.com>
2024-06-19 11:44:26 +0100

    creating state variable step

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
@@ -5,7 +7,7 @@ const messages = [
 ];
 
 export default function App() {
-  const step = 1;
+  const [step, setStep] = useState(1);
 
   function handlePrevious() {
     alert("Previous");
